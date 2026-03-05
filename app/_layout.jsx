5572414d6d155cb21fe9b6dde0e3c0cb0baecc97
marcelofarias2010 <mljinformatica@gmail.com>
2026-03-05 13:57:24 -0300

    aula 02

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,15 @@
-import { Text } from "react-native";
+import { Stack } from "expo-router"
+import { StatusBar } from "expo-status-bar"
+import { colors } from "../constants/colors"
 
 export default function RootLayout() {
-  return <Text>Bom dia</Text>;
+  return (
+    <>
+      <StatusBar backgroundColor={colors.primary} style="light"/>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </>
+  )
 }
